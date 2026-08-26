40345b873e9f768fca9d454be75195ad4eb32fa8
Lvieira82 <97137404+Lvieira82@users.noreply.github.com>
2026-08-26 01:43:43 -0300

    fix: registrar modelos de acesso no app

--- apps/solicitacoes/apps.py
+++ apps/solicitacoes/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class SolicitacoesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.solicitacoes'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.solicitacoes"
+
+    def ready(self):
+        from . import models_acesso  # noqa: F401
