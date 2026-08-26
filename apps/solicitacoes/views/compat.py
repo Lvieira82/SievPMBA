23c085f2e234beeee6763be9a3dba379c31dc667
Lvieira82 <97137404+Lvieira82@users.noreply.github.com>
2026-08-26 01:17:01 -0300

    feat: conectar rotas legadas às funções operacionais

--- apps/solicitacoes/views/compat.py
+++ apps/solicitacoes/views/compat.py
@@ -1,70 +1,19 @@
-from django.http import HttpResponse
-from django.shortcuts import redirect
+"""Compatibilidade das rotas antigas com as views operacionais reais."""
 
-
-def minhas_solicitacoes(request):
-    return redirect("consultar")
-
-
-def _nao_implementada(nome):
-    return HttpResponse(
-        f"A funcionalidade '{nome}' ainda não está disponível nesta versão.",
-        status=501,
-        content_type="text/plain; charset=utf-8",
-    )
-
-
-def verificar_autenticidade(request, *args, **kwargs):
-    return _nao_implementada("verificar autenticidade")
-
-
-def alterar_status(request, *args, **kwargs):
-    return _nao_implementada("alterar status")
-
-
-def documentos_solicitacao(request, *args, **kwargs):
-    return _nao_implementada("documentos da solicitação")
-
-
-def abrir_documento_solicitacao(request, *args, **kwargs):
-    return _nao_implementada("abertura de documento")
-
-
-def lancamento_manual(request, *args, **kwargs):
-    return _nao_implementada("lançamento manual")
-
-
-def opos_geradas(request, *args, **kwargs):
-    return _nao_implementada("OPOs geradas")
-
-
-def detalhe_opo(request, *args, **kwargs):
-    return _nao_implementada("detalhes da OPO")
-
-
-def gerar_opo(request, *args, **kwargs):
-    return _nao_implementada("geração da OPO")
-
-
-def validar_matricula_opo_publica(request, *args, **kwargs):
-    return _nao_implementada("validação de matrícula da OPO")
-
-
-def detalhe_opo_publica(request, *args, **kwargs):
-    return _nao_implementada("detalhes públicos da OPO")
-
-
-def importar_matriculas_painel(request, *args, **kwargs):
-    return _nao_implementada("importação de matrículas")
-
-
-def mapa_eventos(request, *args, **kwargs):
-    return _nao_implementada("mapa de eventos")
-
-
-def gerar_mapa_eventos_pdf(request, *args, **kwargs):
-    return _nao_implementada("PDF do mapa de eventos")
-
-
-def importar_municipios(request, *args, **kwargs):
-    return _nao_implementada("importação de municípios")
+from .operacional import (
+    alterar_status,
+    detalhe_opo,
+    detalhe_opo_publica,
+    documentos_solicitacao,
+    gerar_mapa_eventos_pdf,
+    gerar_opo,
+    importar_matriculas_painel,
+    importar_municipios,
+    lancamento_manual,
+    mapa_eventos,
+    minhas_solicitacoes,
+    opos_geradas,
+    validar_matricula_opo_publica,
+    verificar_autenticidade,
+    abrir_documento_solicitacao,
+)
