90bd7925c72fd6918e98a8fb355bab84a5223180
Lvieira82 <97137404+Lvieira82@users.noreply.github.com>
2026-08-28 18:26:58 -0300

    security: conecta todas as rotas de operação ao escopo

--- apps/solicitacoes/views/__init__.py
+++ apps/solicitacoes/views/__init__.py
@@ -3,10 +3,11 @@ from ..portal_views import agenda_gestao, consultar_protocolo, corrigir_solicita
 from .acesso import esqueci_senha, logout_gestao, redefinir_senha, trocar_senha_primeiro_acesso, verificar_novo_navegador
 from .login_institucional import login_gestao
 from .gestao_acesso import administracao_sistema, usuario_novo, usuario_editar, usuario_senha, usuario_desativar, transferir_solicitacao_segura
-from .administracao import aprovacoes, aprovar_solicitacao, backup, bairros, cprs, configuracoes, municipios, obter_perfil_gestor, painel_administracao, painel_gestao, solicitar_correcao_gestao, tipos_documento, tipos_evento, unidades, usuarios
-from .analise import aprovar, detalhes, estatisticas, fila_analise, historico, indeferir, painel_analise, solicitar_correcao
+from .administracao import backup, bairros, cprs, configuracoes, municipios, obter_perfil_gestor, painel_administracao, painel_gestao, tipos_documento, tipos_evento, unidades, usuarios
+from .seguranca_operacional import aprovacoes_seguras, aprovar_solicitacao_segura, solicitar_correcao_segura, indeferir_seguro, lancamento_manual_seguro
+from .analise import detalhes, estatisticas, fila_analise, historico, painel_analise, solicitar_correcao
 from .compat import alterar_status, detalhe_opo_publica, importar_matriculas_painel, importar_municipios, minhas_solicitacoes, validar_matricula_opo_publica, verificar_autenticidade
-from .operacional import gerar_mapa_eventos_pdf as _gerar_mapa_eventos_pdf_original, lancamento_manual as _lancamento_manual_original
+from .operacional import gerar_mapa_eventos_pdf as _gerar_mapa_eventos_pdf_original
 from .escopo_gestao import documentos_solicitacao_seguro, abrir_documento_solicitacao_seguro, opos_geradas_seguro, detalhe_opo_seguro, gerar_opo_seguro, mapa_eventos_seguro
 from .dashboard import calendario, dashboard, eventos_hoje, mapa, por_municipio, por_tipo, por_unidade
 from .eventos import eventos_dia, eventos_dia_resultado
@@ -24,4 +25,7 @@ detalhe_opo=detalhe_opo_seguro
 gerar_opo=gerar_opo_seguro
 mapa_eventos=mapa_eventos_seguro
 gerar_mapa_eventos_pdf=_gerar_mapa_eventos_pdf_original
-lancamento_manual=_lancamento_manual_original
+lancamento_manual=lancamento_manual_seguro
+aprovacoes=aprovacoes_seguras
+aprovar_solicitacao=aprovar_solicitacao_segura
+solicitar_correcao_gestao=solicitar_correcao_segura
