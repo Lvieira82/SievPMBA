db2efb38138d7320cf175208618b5a8b7da9b441
Lvieira82 <97137404+Lvieira82@users.noreply.github.com>
2026-08-28 18:27:14 -0300

    security: protege também PDF do mapa de eventos

--- apps/solicitacoes/views/__init__.py
+++ apps/solicitacoes/views/__init__.py
@@ -5,10 +5,10 @@ from .login_institucional import login_gestao
 from .gestao_acesso import administracao_sistema, usuario_novo, usuario_editar, usuario_senha, usuario_desativar, transferir_solicitacao_segura
 from .administracao import backup, bairros, cprs, configuracoes, municipios, obter_perfil_gestor, painel_administracao, painel_gestao, tipos_documento, tipos_evento, unidades, usuarios
 from .seguranca_operacional import aprovacoes_seguras, aprovar_solicitacao_segura, solicitar_correcao_segura, indeferir_seguro, lancamento_manual_seguro
-from .analise import detalhes, estatisticas, fila_analise, historico, painel_analise, solicitar_correcao
+from .analise import detalhes, estatisticas, fila_analise, historico, painel_analise, solicitar_correcao, aprovar as aprovar_analise
 from .compat import alterar_status, detalhe_opo_publica, importar_matriculas_painel, importar_municipios, minhas_solicitacoes, validar_matricula_opo_publica, verificar_autenticidade
-from .operacional import gerar_mapa_eventos_pdf as _gerar_mapa_eventos_pdf_original
 from .escopo_gestao import documentos_solicitacao_seguro, abrir_documento_solicitacao_seguro, opos_geradas_seguro, detalhe_opo_seguro, gerar_opo_seguro, mapa_eventos_seguro
+from .mapa_seguro import gerar_mapa_eventos_pdf_seguro
 from .dashboard import calendario, dashboard, eventos_hoje, mapa, por_municipio, por_tipo, por_unidade
 from .eventos import eventos_dia, eventos_dia_resultado
 from .protocolo import cancelar_protocolo, detalhes_protocolo, encaminhar_unidade, estatisticas_protocolo, fila_protocolo, historico_protocolo, painel_protocolo, reenviar_email
@@ -24,8 +24,10 @@ opos_geradas=opos_geradas_seguro
 detalhe_opo=detalhe_opo_seguro
 gerar_opo=gerar_opo_seguro
 mapa_eventos=mapa_eventos_seguro
-gerar_mapa_eventos_pdf=_gerar_mapa_eventos_pdf_original
+gerar_mapa_eventos_pdf=gerar_mapa_eventos_pdf_seguro
 lancamento_manual=lancamento_manual_seguro
 aprovacoes=aprovacoes_seguras
 aprovar_solicitacao=aprovar_solicitacao_segura
 solicitar_correcao_gestao=solicitar_correcao_segura
+indeferir_solicitacao=indeferir_seguro
+aprovar=aprovar_analise
