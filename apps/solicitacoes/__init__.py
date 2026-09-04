45dae2199c3e62ae292a67b7e663ab5d8af4bce0
Lvieira82 <97137404+Lvieira82@users.noreply.github.com>
2026-09-04 13:33:20 -0300

    Correct solicitacoes initializer to match siev95.3 source

--- apps/solicitacoes/__init__.py
+++ apps/solicitacoes/__init__.py
@@ -1,1 +1,6 @@
-from .models import *
+from django.apps import AppConfig
+
+
+class SolicitacoesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.solicitacoes' 
