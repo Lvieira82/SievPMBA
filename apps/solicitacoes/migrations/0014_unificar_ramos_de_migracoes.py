762899aef42452ad7b7275fd1ba7e7f1d0a0a4f7
Lvieira82 <97137404+Lvieira82@users.noreply.github.com>
2026-09-05 12:44:48 -0300

    Unificar ramos de migrations

---
+++ apps/solicitacoes/migrations/0014_unificar_ramos_de_migracoes.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("solicitacoes", "0012_aplicar_areas_responsabilidade_csv"),
+        ("solicitacoes", "0013_cadastrar_unidades_bairros_feira_santana"),
+    ]
+
+    operations = []
