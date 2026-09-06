771c0b796ce8ed755845a0a80541e1df3c72737a
Lvieira82 <97137404+Lvieira82@users.noreply.github.com>
2026-09-05 21:43:59 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
 EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="adm95cipm@gmail.com")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default='vsihbwbmxwdxbheh')
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="vsihbwbmxwdxbheh")
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
 EMAIL_USE_SSL = config("EMAIL_USE_SSL", default=False, cast=bool)
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="adm95cipm@gmail.com")
