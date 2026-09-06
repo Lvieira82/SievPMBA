64c812f9288e0cc6a5d80e3e1b71e68a5235d844
Lvieira82 <97137404+Lvieira82@users.noreply.github.com>
2026-09-05 21:32:50 -0300

    configuração so SMTP

--- config/settings.py
+++ config/settings.py
@@ -76,13 +76,13 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # Mesmo serviço SMTP utilizado pelo siev95.3.
 # A senha fica exclusivamente nas variáveis de ambiente do Render.
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
-EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="adm95cipm@gmail.com")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
-EMAIL_USE_SSL = config("EMAIL_USE_SSL", default=False, cast=bool)
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="adm95cipm@gmail.com")
+EMAIL_HOST =smtp.gmail.com
+EMAIL_PORT =587
+EMAIL_HOST_USER =adm95cipm@gmail.com
+EMAIL_HOST_PASSWORD ='vsihbwbmxwdxbheh'
+EMAIL_USE_TLS =True
+EMAIL_USE_SSL =False
+DEFAULT_FROM_EMAIL =adm95cipm@gmail.com
 USER_EMAIL_FROM = config("USER_EMAIL_FROM", default=DEFAULT_FROM_EMAIL)
 EMAIL_TIMEOUT = config("EMAIL_TIMEOUT", default=20, cast=int)
 
